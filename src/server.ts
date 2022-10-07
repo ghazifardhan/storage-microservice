3feec17378dc4592207bdf1befca6e696397b0be
Ghazi Fadil <ghazi@solusinet.id>
2022-10-07 13:47:15 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -21,6 +21,11 @@ app.use(morgan('combined'));
 app.use(cors({
   origin: '*'
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
